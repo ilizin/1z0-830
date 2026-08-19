440485e3ce69cbe772e7f74f4e14a0a6711e6ac9
ilizin <ilio.zincone@gmx.com>
2026-08-19 23:04:32 +0200

    Progressing with Array (6)

--- src/main/java/me/ilizin/certification/array/CompareAndMismatch.java
+++ src/main/java/me/ilizin/certification/array/CompareAndMismatch.java
@@ -1,4 +1,57 @@
 package me.ilizin.certification.array;
 
+import java.util.Arrays;
+
 public class CompareAndMismatch {
+
+    public static void main(String[] args) {
+        /* Finds and returns the index of the first mismatch between two int arrays, otherwise return -1 if no mismatch
+           is found. The index will be in the range of 0 (inclusive) up to the length (inclusive) of the smaller array.
+
+           If the two arrays share a common prefix then the returned index is the length of the common prefix and
+           it follows that there is a mismatch between the two elements at that index within the respective arrays.
+           If one array is a proper prefix of the other then the returned index is the length of the smaller array and
+           it follows that the index is only valid for the larger array. Otherwise, there is no mismatch. */
+
+        int[] ia1 = { 0, 1, 2, 6 };
+        int[] ia2 = { 0, 1, 5 };
+        System.out.println(Arrays.mismatch(ia1, ia2)); // prints 2
+
+        ia1 = new int[]{ 0, 1, 5, 6 };
+        ia2 = new int[]{ 0, 1, 5 };
+        System.out.println(Arrays.mismatch(ia1, ia2)); // prints 3
+
+        ia1 = new int[0];
+        ia2 = new int[0];
+        System.out.println(Arrays.mismatch(ia1, ia2)); // prints -1
+
+        ia1 = new int[]{ 0, 1, 5, 6 };
+        ia2 = new int[]{ 1, 1, 5, 6 };
+        System.out.println(Arrays.mismatch(ia1, ia2)); // prints 0
+
+        ia1 = new int[]{ 0, 1, 5, 6 };
+        ia2 = new int[]{ 0, 1, 5, 6 };
+        System.out.println(Arrays.mismatch(ia1, ia2)); // prints -1
+
+        ia1 = new int[]{ 0, 1, 5, 6 };
+        ia2 = new int[]{ 0, 1, 5, 7 };
+        System.out.println(Arrays.mismatch(ia1, ia2)); // prints 3
+
+        /* Compares two int arrays lexicographically. If the two arrays share a common prefix then the lexicographic
+           comparison is the result of comparing two elements, as if by Integer.compare(int, int), at an index within
+           the respective arrays that is the prefix length. Otherwise, one array is a proper prefix of the other and,
+           lexicographic comparison is the result of comparing the two array lengths.
+           A null array reference is considered lexicographically less than a non-null array reference. Two null array
+           references are considered equal. It returns 0 if the first and second array are equal and contain the same
+           elements in the same order; a value less than 0 if the first array is lexicographically less than the
+           second array; and a value greater than 0 if the first array is lexicographically greater than the second array. */
+        ia1 = new int[]{ 0, 1, 2, 6};
+        ia2 = new int[]{ 0, 1, 5};
+        System.out.print(Arrays.compare(ia1, ia2)); //prints 3
+
+        ia1 = new int[]{ 0, 1, 5, 6};
+        ia2 = new int[]{ 0, 1, 5};
+        System.out.print(Arrays.compare(ia1, ia2)); //prints -11
+
+    }
 }
--- src/main/java/me/ilizin/certification/io/FileInputAndOutputStream.java
+++ src/main/java/me/ilizin/certification/io/FileInputAndOutputStream.java
@@ -2,13 +2,21 @@ package me.ilizin.certification.io;
 
 import java.io.*;
 
-public class Test {
+public class FileInputAndOutputStream {
 
-    /* There are several specialized exception classes such as FileNotFoundException or EOFException but all of them
+    /*  There are several specialized exception classes such as FileNotFoundException or EOFException but all of them
         are rooted under the checked exception class java.io.IOException
         FileInputStream/FileOutputStream throws file with the specified pathname does not exist. It will also be
         thrown by these constructors if the file does exist but for some reason is inaccessible, for example
-        when an attempt is made to open a read-only file for writing */
+        when an attempt is made to open a read-only file for writing
+
+        For a long time, Java had only the classes in java.io package to perform I/O related tasks. However,
+        these classes implement what is known as "blocking" I/O operations.
+        If you try to read data from a remote file or from a network socket, the thread on which the read method
+        is being executed may be blocked for a long time and will not be available to perform other tasks.
+        Although this approach is fine for many applications, it doesn't scale well. To overcome this limitation,
+        Java introduced a non-blocking I/O (aka NIO) library in Java 1.4 and updated it in Java 7 (aka NIO2).
+        These classes are packaged in the java.nio package.*/
     public static void main(String[] args) throws IOException {
 
         /* The concept of raw input stream and raw output stream is represented by abstract classes InputStream and
@@ -48,11 +56,49 @@ public class Test {
         /* The read method populates the byte array with the bytes read from the input stream and returns the number
            of bytes that it actually read. It overwrites the byte array with the newly read bytes starting from
            index 0. */
+
         while( (bytesRead = is.read(chunk)) != -1 ) {
+            /* The available() method of FileInputStream class is used to return the estimated number of remaining
+               bytes that can be read from the input stream without blocking. This method returns the number of bytes
+               remaining to read from the file. When a file is completely read, this function returns zero. */
+            System.out.print(is.available());
             /* To write the bytes to the output stream, we are using OutputStream's
             write(byte[] b, int offset, int len) method, which writes len number of bytes from the given byte
             array starting from the index given by the offset parameter */
             os.write(chunk, 0, bytesRead);
         };
     }
+
+    /* abstract class InputStream : The root class for all byte based input streams
+       Important methods:
+        int available()
+        int read()
+        int read(byte[] b)
+        int read(byte[] b, int off, int len)
+        byte[] readAllBytes()
+        void reset()
+        long skip(long n)
+        void mark(int readlimit)
+        boolean markSupported()
+        void close()
+
+       class FileInputStream : Used to read bytes from a file
+       Important constructors
+        FileInputStream(String name)
+        FileInputStream(File f)
+
+       abstract class OutputStream : The root class for all byte based output streams
+       Important methods:
+        void write(byte[] b)
+        void write(byte[] b, int off, int len)
+        void write(int b)
+        void flush()
+        void close()
+
+       class FileOutputStream : Used to write bytes to a file
+       Important constructors
+        FileOutputStream(String name)
+        FileOutputStream(File f)
+        FileOutputStream(String name, boolean append)
+        FileOutputStream(File f, boolean append) */
 }
