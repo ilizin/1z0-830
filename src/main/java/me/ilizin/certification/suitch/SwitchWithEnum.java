5e9cd2f817a8e763d25a5bce3864d92ae93a4e44
ilizin <ilio.zincone@gmx.com>
2026-08-21 23:41:35 +0200

    Improve SwitchWithEnum class

--- src/main/java/me/ilizin/certification/suitch/SwitchWithEnum.java
+++ src/main/java/me/ilizin/certification/suitch/SwitchWithEnum.java
@@ -7,14 +7,15 @@ public class SwitchWithEnum {
 
     public static void main(String[] args) {
 
-        // Enums are used very often in a switch statement.
+        System.out.println();
+        /* The enums are used very often in a switch statement. */
         DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
         switch (dayOfWeek) {
             case MONDAY:
                 System.out.println("Today is Monday");
                 break;
-            /* Prior to Java 21, the case statements could only use constant without the enum name,
-               so this would cause a compilation error. */
+            /* Prior to Java 21, the case statements could only use constant without the enum name, so this would cause
+               a compilation error. */
             case DayOfWeek.TUESDAY:
                 System.out.println("Today is Tuesday");
                 break;
@@ -22,16 +23,19 @@ public class SwitchWithEnum {
                 System.out.println("Today is a different day");
         }
 
-        dayOfWeek = DayOfWeek.THURSDAY; // It prints "Today is Thursday, Saturday or Sunday"
+        System.out.println();
+        /* It prints "Today is Thursday, Saturday or Sunday" */
+        dayOfWeek = DayOfWeek.THURSDAY;
         switch (dayOfWeek) {
             case MONDAY, TUESDAY -> System.out.println("Today is Monday or Tuesday");
             case WEDNESDAY, FRIDAY -> System.out.println("Today is Wednesday or Friday");
             default -> System.out.println("Today is Thursday, Saturday or Sunday");
         }
 
-        dayOfWeek = DayOfWeek.TUESDAY; // It prints "Today is Thursday, Saturday or Sunday"
+        System.out.println();
+        /* It prints "Today is Thursday, Saturday or Sunday", TUESDAY is just a label applied to the print statement */
+        dayOfWeek = DayOfWeek.TUESDAY;
         switch (dayOfWeek) {
-            // TUESDAY is just a label applied to the print statement.
             case MONDAY: TUESDAY:
                 System.out.println("Today is Monday or Tuesday");
                 break;
