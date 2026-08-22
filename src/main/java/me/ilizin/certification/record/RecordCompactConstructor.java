4a55f27ea2be03ce0c90a4632c8ca72c994c4541
ilizin <ilio.zincone@gmx.com>
2026-08-22 19:30:25 +0200

    Improve RecordCompactConstructor class (2)

--- src/main/java/me/ilizin/certification/record/RecordCompactConstructor.java
+++ src/main/java/me/ilizin/certification/record/RecordCompactConstructor.java
@@ -3,15 +3,14 @@ package me.ilizin.certification.record;
 public class RecordCompactConstructor {
 
     /* There's a new shorter way of writing the canonical constructor, called the "compact constructor".
-       Exactly one constructor, either long-form or compact, not both. */
+       Exactly one canonical constructor, either long-form or compact, not both. */
     public record Student(int id, String name, String address) {
 
         /* There is no formal parameter list in the compact constructor, the compiler just copies it from
            the one present in the record header. We are not assigning the value of the variables to the record fields,
            there is no this.id = id actually, you are not allowed to set or modify the component fields explicitly at all
-           anywhere in the compact constructor.
-           Component fields are final, and they are set only once at the end of the compact constructor by code
-           inserted by the compiler. */
+           anywhere in the compact constructor, component fields are final, and they are set only once at the end of the
+           compact constructor by code inserted by the compiler. */
         public Student {
             if (id < 1) throw new RuntimeException("Invalid ID");
             if (name.length() < 0) {
