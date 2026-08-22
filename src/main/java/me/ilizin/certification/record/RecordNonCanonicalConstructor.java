b758e41b88ca4f094a1247e7c803c3aac23078f7
ilizin <ilio.zincone@gmx.com>
2026-08-22 19:53:45 +0200

    Improve RecordNonCanonicalConstructor class

--- src/main/java/me/ilizin/certification/record/RecordNonCanonicalConstructor.java
+++ src/main/java/me/ilizin/certification/record/RecordNonCanonicalConstructor.java
@@ -9,19 +9,18 @@ public class RecordNonCanonicalConstructor {
            "alternate constructor"). An implication of this requirement is that by the time the control reaches
            the second line of non-canonical constructor's body, the component fields of this record are already set.
            Since the component fields are final, you can't modify them anymore.
-           Name of the parameter in a non-canonical constructor doesn't have to be the same as the
-           name of the component field. */
+           Name of the parameter in a non-canonical constructor doesn't have to be the same as the name of the component
+           field. */
         public Student(int whatever, String name) throws Exception { // Throws clause is allowed.
-            /* I haven't provided a canonical constructor in the above record and so the compiler will provide
-               it automatically, so the call to this(...) is valid.
-               It is possible to validate input arguments or to supply normalized values of input arguments to the
-               alternate constructor */
+            /* I haven't provided a canonical constructor, so the compiler will provide it automatically, so the call to
+               this(...) is valid. It is possible to validate input arguments or to supply normalized values of input
+               arguments to the alternate constructor */
             // this(0); Not allowed, it's detected as a recursive constructor call
-            this(checkId(whatever), name.trim(), "DUMMY"); //first line must be an explicit call to another constructor of this record
-            // component fields of this record are already set at this point
-            // this.name = "Dummy"; //can't do this now
-            name = "Dummy"; /* Valid but name refers to the constructor parameter and updating it has no
-                               impact on the component field */
+            this(checkId(whatever), name.trim(), "DUMMY");
+            /* component fields of this record are already set at this point */
+            // this.name = "Dummy";
+            /* Valid but name refers to the constructor parameter and updating it has no impact on the component field */
+            name = "Dummy";
         }
 
         public Student(int whatever) throws Exception {
@@ -34,5 +33,11 @@ public class RecordNonCanonicalConstructor {
             }
             return value;
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+        Student student = new Student(1, "John", "Main square");
+        System.out.println(student);
     }
 }
