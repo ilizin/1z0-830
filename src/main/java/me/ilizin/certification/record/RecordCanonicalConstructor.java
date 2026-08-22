583fd6de86b843a6775c010b016e13341c6cfb6e
ilizin <ilio.zincone@gmx.com>
2026-08-22 19:23:30 +0200

    Improve RecordCanonicalConstructor class

--- src/main/java/me/ilizin/certification/record/RecordCanonicalConstructor.java
+++ src/main/java/me/ilizin/certification/record/RecordCanonicalConstructor.java
@@ -4,17 +4,16 @@ public class RecordCanonicalConstructor {
 
     public record Student(int id, String name, String address) {
 
-        /* Every record must have exactly one canonical constructor. If you don't provide it in your record explicitly,
-           the compiler will provide it. Unlike the default constructor of a regular class, the canonical
+        /* Every record must have exactly one canonical constructor, if you don't provide it in your record explicitly,
+           the compiler will provide it for you. Unlike the default constructor of a regular class, the canonical
            constructor must initialize all of the component fields of the record using the parameters passed to it.
            It has no throws clause and its access modifier is the same as that of the record itself, not more restrictive.
            It is possible defining the canonical constructor explicitly, the order, the names, and the types must
-           match with the record header. Initialization of component fields must be done explicitly.
-           Explicit calls to this(...) or super(...) are not allowed. */
-
-        public Student(int id, String name, String address) {
+           match with the record header. Initialization of component fields must be done explicitly, and explicit calls
+           to this(...) or super(...) are not allowed. */
+        public Student(int id, String name, String address) { // protected is not allowed because of the record is public
             // Throws clause is not allowed. Body may throw unchecked exceptions.
-            if (id < 1) throw new RuntimeException("Invalid ID"); //check validity of input
+            if (id < 1) throw new RuntimeException("Invalid ID");
             this.id = id;
             if (name.trim().length() == 0) name = "DUMMY"; //fix input data
             this.name = name; // commenting this will generate a compilation error
@@ -23,6 +22,7 @@ public class RecordCanonicalConstructor {
     }
 
     public static void main(String[] args) {
+        System.out.println();
         Student student = new Student(1, "John", "Main square");
         System.out.println(student);
     }
