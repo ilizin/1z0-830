14d12123a34920ba09a3a0496f7a3f062f3f6b9a
ilizin <ilio.zincone@gmx.com>
2026-08-26 22:07:10 +0200

    Improve InterfaceDefinition class

--- src/main/java/me/ilizin/certification/interfaces/otherpackage/InterfaceApp.java
+++ src/main/java/me/ilizin/certification/interfaces/otherpackage/InterfaceApp.java
@@ -1,9 +1,9 @@
 package me.ilizin.certification.interfaces.otherpackage;
 
-public class Interface {
+public class InterfaceApp {
 
     public static void main(String[] args) {
         // me.ilizin.certification.interfaces.Interface.MovableNoPublic movableNoPublic;
-        me.ilizin.certification.interfaces.Interface.Movable movable;
+        me.ilizin.certification.interfaces.InterfaceDefinition.Movable movable;
     }
 }
