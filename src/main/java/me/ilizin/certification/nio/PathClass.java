0d5911a31821cf15574d524dcfbd66a7aea0e833
ilizin <ilio.zincone@gmx.com>
2026-09-07 22:23:22 +0200

    Progressing nio (2)

--- src/main/java/me/ilizin/certification/nio/PathClass.java
+++ src/main/java/me/ilizin/certification/nio/PathClass.java
@@ -17,13 +17,30 @@ public class PathClass {
            it doesn't start with a root component. It will be interpreted as being relative to the current directory,
            which is C:\temp in this case. */
         Path currentDirPath = Path.of("./files");
-        System.out.println(currentDirPath.getFileName()); // files
-        System.out.println(currentDirPath.subpath(1, 2)); // files
-        System.out.println(currentDirPath.getRoot()); // null
+        /* The getFileName method returns the farthest (i.e. the last) element of the path, which is files in this case. */
+        System.out.println(currentDirPath.getFileName());
+        /* The returned Path object has the name elements that begin at beginIndex and extend to the element at index endIndex-1.
+           Note that it returns a relative path, which means, the returned path will never start with the root. */
+        System.out.println(currentDirPath.subpath(1, 2));
+        /* The getRoot method returns the root if it is invoked on an absolute path. Since ./files is not an absolute path,
+           getRoot returns null here. */
+        System.out.println(currentDirPath.getRoot());
+        /* The toAbsolutePath() method returns the full path of the given relative path starting from the default root.
+           In this case, the root is C:\ and so, it returns C:\temp\.\files. Observe that this path contains a ., which
+           is redundant. This shows that the toAbsolutePath method does not return the canonical version of the path. */
         Path absPath = currentDirPath.toAbsolutePath();
         System.out.println(absPath); // C:\temp\.\files
+        /* To get that, we need to call normalize(). The normalize() method is practically the same as the getCanonicalPath
+           method of the java.io.File class (except the fact that normalize() returns a Path object instead of a String). */
         Path normalizedCurrentDirPath = absPath.normalize();
         System.out.println(normalizedCurrentDirPath); // C:\temp\files
+        /* The toRealPath method returns the canonical path for the given path and also checks if the file (or the directory)
+           referred to by the path actually exists. If the file does not exist, it throws an IOException. Thus, in this example,
+           if the files directory exists, it will return C:\temp\file sand if it does not, it will throw an IOException. */
         System.out.print(normalizedCurrentDirPath.toRealPath()); // java.nio.file.NoSuchFileException
+
+        /* A well formed paths do not end with a trailing separator character (unless it is referring to the root such as C:\ or /).
+           So, even if you try to create a Path with a trailing slash, it will be ignored. For example:
+           System.out.print(Path.of("/a/b/")); will print /a/band not /a/b/. */
     }
 }
