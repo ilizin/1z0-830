1918eb53addce1adedcb90de30218d40f5a71fa1
ilizin <ilio.zincone@gmx.com>
2026-09-20 09:27:33 +0200

    Documenting String (2)

--- src/main/java/me/ilizin/certification/strings/CreatingStrings.java
+++ src/main/java/me/ilizin/certification/strings/CreatingStrings.java
@@ -32,5 +32,59 @@ public class CreatingStrings {
            Therefore, you cannot apply the array indexing operator on a string. */
         String str = new String("Hello");
         //char c = str[0]; // Invalid
+
+        System.out.println();
+        System.out.println("*** PART 2 ***");
+        /* Strings are used so commonly in programs that creating strings using the constructor becomes too tedious.
+           So, Java allows you to create a string by just specifying the contents of the string within double quotes.
+           They are called single line string literals because you have to specify the complete contents of the string
+           in a single line of code. You cannot split the contents across multiple lines. */
+        String word6 = "Double quote \", Tab \t, New line,\nEnd";
+        System.out.println(str);
+        /* String word7 = "Double quote \", Tab \t, New line, // Invalid
+        End"; */
+
+        System.out.println();
+        System.out.println("*** PART 3 ***");
+        /* The second common way of creating strings is by using the concatenation operator: +
+           The + operator is overloaded in such a way that if either one of its two operands is a string, it converts the
+           other operand to a string and produces a new string by joining the two. There is no restriction on the type of
+           operands as long as one of them is a string. */
+        String word8 = "hello ";
+        String word9 = word8 + " world";
+        System.out.println(word9);
+        /* 1. If the non-string operand is a reference variable, the toString() method is invoked on that reference to get a
+           string representation of that object.
+           2. If the non-string operand is a primitive variable or a primitive literal value, a wrapper object of the same
+           type is created using the primitive value and then a string representation is obtained by invoking toString()
+           on the wrapper object.
+           3. If the one of the operands is a null literal or a null reference variable, the string "null" is used instead
+           of invoking any method on it. */
+        String s1 = "hello ";
+        String s11 = s1 + 1; //produces "hello 1"
+        String s12 = 1 + " hello"; //produces "1 hello"
+        String s2 = "" + true; //produces "true";
+        double d = 0.0;
+        String s3 =  "-" + d +"-"; //produces "-0.0-"
+        Object o = null;
+        String s4 = "hello " + o; //produces "hello null". No NullPointerException here.
+        /* Just like a mathematical expression involving the + operator, string concatenation is also evaluated from left to right.
+           Therefore, while evaluating the expression "1"+2+3, "1"+2 is evaluated first to produce "12" and then "12"+3 is evaluated
+           to produce "123". On the other hand, the expression 1 + 2 + "3" produces "33". */
+        String word10 = "1" + 2 + 3;
+        System.out.println(word10);
+        String word11 = 1 + 2 + "3";
+        System.out.println(word11);
+        /* At least one of its operands must be a String. */
+        //String x = true + 1; // Invalid
+        Object obj = "string";
+        /* Will not compile. Even though obj points to a String at runtime, as far as the compiler is concerned, obj is an
+           Object and not a String. */
+        // String y = obj + obj;
+
+        System.out.println();
+        System.out.println("*** PART 4 ***");
+        
+
     }
 }
